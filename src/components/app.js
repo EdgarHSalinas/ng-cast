62b6b1060944494352bf987411d2ca6a51530dca
Jonathan Pizzolato <jpizzolato36@gmail.com>
2018-05-11 14:34:50 -0700

    Angular

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
-angular.module('video-player')
-
-.component('app', {
-  // TODO
+angular.module('video-player', [])
+.component('app', { //configuration object
+  controller: () => { return 'some function' },
+  templateUrl: 'app.html' //replace with html...or switch back to the template Url key.
 });
